3bd1d840974c4c94b692ec0fe1ca73a5301e6cb9
Sheekha Jariwala <jariwals@oregonstate.edu>
2020-08-14 23:27:54 -0700

    Added more routes to the app

--- flask-sample/helloworld.py
+++ flask-sample/helloworld.py
@@ -2,5 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def helloWorld():
-    return 'Hello World!'
+def index():
+    return 'Index page!'
+
+@app.route('/hello')
+def hello():
+    return 'Hello World page!'
