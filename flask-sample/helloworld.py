2ec4cb395e2a194cbc57aff5d1a094fd1623df40
Sheekha Jariwala <jariwals@oregonstate.edu>
2020-08-17 23:48:19 -0700

    Add variables to decorator and pass as argument to method

--- flask-sample/helloworld.py
+++ flask-sample/helloworld.py
@@ -8,3 +8,11 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello World page!'
+
+@app.route('/user/generic/<username>')
+def show_user_profile(username):
+    return f'Username is {username}. len - {len(username)}'
+
+@app.route('/user/string/<string:username>')
+def show_user_profile_str(username):
+    return f'Username is {username}. len - {len(username)} type is {type(username)}'
