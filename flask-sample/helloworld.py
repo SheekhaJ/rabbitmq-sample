ff1031db98a16373071a805010303de307ef41ba
Sheekha Jariwala <jariwals@oregonstate.edu>
2020-08-17 23:58:42 -0700

    Added GET and POST method types

--- flask-sample/helloworld.py
+++ flask-sample/helloworld.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,3 +18,10 @@ def show_user_profile(username):
 @app.route('/user/string/<string:username>')
 def show_user_profile_str(username):
     return f'Username is {username}. len - {len(username)} type is {type(username)}'
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        return f'Called POST method on /login route {request.method}'
+    else:
+        return f'Called GET method on /login route {request.method}'
