5a9840684c83db5a2183db2bddd888b0f2e8c5b3
Sheekha Jariwala <jariwals@oregonstate.edu>
2020-08-31 23:45:29 -0700

    Run Flask app using app.run()

--- flask-pub-sub/flask-publisher.py
+++ flask-pub-sub/flask-publisher.py
@@ -7,12 +7,17 @@ exchangeName = 'cmds'
 
 @app.route('/cmds/<cmd>', methods=['GET'])
 def addCmd(cmd):
-    # if request.method == 'GET':
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
-    channel.exchange_declare(exchange=exchangeName, exchange_type='fanout')
-    print(f'printing received command - {cmd}')
-    channel.basic_publish(exchange=exchangeName, routing_key='', body=cmd)
-    connection.close()
-    return '[x] Sent cmd {}'.format(cmd)
+    if request.method == 'GET':
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+        channel.exchange_declare(exchange=exchangeName, exchange_type='fanout')
+        channel.basic_publish(exchange=exchangeName, routing_key='', body=cmd)
+        connection.close()
+        return '[x] Sent cmd {}'.format(cmd)
 
+@app.route('/')
+def hello():
+    return 'OK'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
