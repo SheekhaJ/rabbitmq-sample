c74cb96b418d001da0e82bc295f5f30e20171c4a
Sheekha Jariwala <jariwals@oregonstate.edu>
2020-09-04 23:22:27 -0700

    Clarification regarding usage of trailing slashes in URLs

--- flask-sample/helloworld.py
+++ flask-sample/helloworld.py
@@ -25,3 +25,17 @@ def login():
         return f'Called POST method on /login route {request.method}'
     else:
         return f'Called GET method on /login route {request.method}'
+
+# Accessing the URL with a trailing / or without will redirect to the loginwithslash page
+@app.route('/loginwithslash/',methods=['GET'])
+def loginwithslash():
+    return f'Called GET method on /loginwithslash/ route {request.method}'
+
+# Accessing the URL with a trailing / here will access the /loginwithslash/1 page 
+# but without a trailing / will give 404 because the route has been mentioned with a trailing /
+@app.route('/loginwithslash/1/',methods=['GET'])
+def loginwithslash1():
+    return f'Called GET method on /loginwithslash/1/ route {request.method}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port='5001',debug=True)
